c7428f3f34249651c2c66d36707ca90917fab3f7
Emily Ding <emilyd61@uw.edu>
2018-07-17 19:53:57 -0700

    money pool list in react

--- src/components/home-page/HomePage.js
+++ src/components/home-page/HomePage.js
@@ -7,14 +7,18 @@ import firebase from 'firebase/app';
 class HomePage extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = { };
     }
 
     componentDidMount() {
+        
+
         this.userProfilesRef = firebase.database().ref('users');
         this.userProfilesRef.on('value', (snapshot) => {
 
-            // this is all our user data. look in the console for structure/names/types/etc. the important parts for the pool are the name, item, and price.
+            
+            // this is all our user data. look in the console for structure/names/types/etc. 
+            // the important parts for the pool are the name, item, and price.
             // ill push changes when im done with the item, shouldnt take more than 20 mins but i want you 
             // to start this asap. youre going to want to use a mapping function 
             // (to the effect of: this.state.userProfilesData.map((userData) => <RowInOurTable />)
@@ -23,7 +27,25 @@ class HomePage extends Component {
             // dont worry about "commit purchase" yet.
             // text if you have questions <3
             console.log(snapshot.val());
-            this.setState({userProfilesData: snapshot.val()});
+            
+            let contributions = [];
+            let usersObject = snapshot.val(); //convert snapshot to value
+            let usersKeys = Object.keys(usersObject);
+            let usersArray = usersKeys.map((key) => { //map array of keys into array of tasks
+            
+                let user = usersObject[key]; //access element at that key
+                user.key = key;
+                return user; //the transformed object to store in the array
+            });
+
+            usersArray.forEach( (user) => {
+                if (user.contribution) {
+                    contributions.push(user);
+                }
+
+            });
+
+            this.setState({commitUsers: contributions});
         });
         
     }
@@ -34,36 +56,50 @@ class HomePage extends Component {
 
     render() {
 
-        return (
-            <div class="col-sm" id="pool">
-            <h2>Fund Pool</h2>
-            <div id="total">TOTAL: $555555.99/wk</div>
-                <table class="table table-striped table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Commitment</th>
-                            <th scope="col"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row" class="align-middle">1</th>
-                            <td class="align-middle">Person A</td>
-                            <td class="align-middle">$$$ Wk</td>
-                            <td class="align-middle"><Link to="/personal-chat" class="btn btn-secondary">Message</Link></td>
-                        </tr>
-                        <tr>
-                            <th scope="row" class="align-middle">1</th>
-                            <td class="align-middle">Person A</td>
-                            <td class="align-middle">$$$ Wk</td>
-                            <td class="align-middle"><Link to="/personal-chat" class="btn btn-secondary">Message</Link></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        );
+        if (this.state.commitUsers) {
+
+            let contributions = this.state.commitUsers.map((user)=> {
+                return <CommitmentRow name={user.name} contribution={user.contribution} key={user.key} />
+            });
+
+            return (
+                <div className="col-sm" id="pool">
+                <h2>Fund Pool</h2>
+                <div id="total">TOTAL: $555555.99/wk</div>
+                    <table className="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Commitment</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {contributions}
+
+                        </tbody>
+                    </table>
+                </div>
+            );
+        }
+        else {
+            return(<div>Loading... </div>);
+        }
     }
 }
+
+class CommitmentRow extends Component {
+    
+    render() {
+
+        return (<tr>
+            {/* <th scope="row" className="align-middle">1</th> */}
+            <td className="align-middle">{this.props.name}</td>
+            <td className="align-middle">{this.props.contribution}</td>
+            <td className="align-middle"><Link to="/personal-chat" className="btn btn-secondary">Message</Link></td>
+        </tr>);
+    }
+}
+
 
 export default HomePage;

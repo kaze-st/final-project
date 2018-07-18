5d26f4f723f98fdddf08f6baf539abdc5a7392ff
Hao Chen <haoc46@cs.washington.edu>
2018-07-18 15:39:35 -0700

    Responsive buttons!

--- src/components/profile/ProfilePage.js
+++ src/components/profile/ProfilePage.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { Button } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
-import { TradeItemList } from './TradeOffers';
+import React, {Component} from 'react';
+import {Button} from 'reactstrap';
+import {NavLink} from 'react-router-dom';
+import {TradeItemList} from './TradeOffers';
 
 import firebase from 'firebase/app';
 
@@ -14,13 +14,13 @@ import firebase from 'firebase/app';
 class ProfilePage extends Component {
     constructor(props) {
         super(props);
-        this.state = { userProfileData: undefined };
+        this.state = {userProfileData: undefined};
     }
 
     componentDidMount() {
         this.userProfileRef = firebase.database().ref('users').child(this.props.match.params.uid);
         this.userProfileRef.on('value', (snapshot) => {
-            this.setState({ userProfileData: snapshot.val() });
+            this.setState({userProfileData: snapshot.val()});
         });
     }
 
@@ -29,28 +29,36 @@ class ProfilePage extends Component {
     }
 
     render() {
-        if (!this.state.userProfileData) { return null };
+        if (!this.state.userProfileData) {
+            return null
+        }
+
 
         let button = null;
         if (this.props.currentUser.uid === this.props.match.params.uid) {
-            button = <Button color="primary">
+            button =
                 <NavLink to={"/profile/" + this.props.match.params.uid + "/edit"}>
-                    Edit Profile
-                        </NavLink>
-            </Button>;
+                    <Button color="primary">
+                        Edit Profile</Button>
+                </NavLink>
+
         } else {
             button = (
                 <div className="row">
-                    <Button color="primary">
-                        <NavLink to={"/personal-chat"}>
+
+                    <NavLink to={"/personal-chat"}>
+                        <Button color="primary">
                             {"Chat with " + this.state.userProfileData.handle}
-                        </NavLink>
-                    </Button>
-                    <Button color="primary">
-                        <NavLink to={"/trade"}>
+                        </Button>
+                    </NavLink>
+
+
+                    <NavLink to={"/trade"}>
+                        <Button color="primary">
                             {"Trade with " + this.state.userProfileData.handle}
-                        </NavLink>
-                    </Button>
+                        </Button>
+                    </NavLink>
+
                 </div>);
         }
 
@@ -62,14 +70,18 @@ class ProfilePage extends Component {
                     <div id="profileHeader">
                         <h2>My Profile Page</h2>
                     </div>
-                    {this.state.userProfileData.avatar && <img src={this.state.userProfileData.avatar} alt="profile avatar" id="profileImage" class="img-fluid" />}
+                    {this.state.userProfileData.avatar &&
+                    <img src={this.state.userProfileData.avatar} alt="profile avatar" id="profileImage" class="img-fluid"/>}
                     <div id="profileInfo">
-                        {this.state.userProfileData.name && <p className="text-center"><strong>Full name: </strong>{this.state.userProfileData.name}</p>}
-                        {this.state.userProfileData.email && <p className="text-center"><strong>E-Mail: </strong>{this.state.userProfileData.email}</p>}
-                        {this.state.userProfileData.handle && <p className="text-center"><strong>Handle: </strong>{this.state.userProfileData.handle}</p>}
+                        {this.state.userProfileData.name &&
+                        <p className="text-center"><strong>Full name: </strong>{this.state.userProfileData.name}</p>}
+                        {this.state.userProfileData.email &&
+                        <p className="text-center"><strong>E-Mail: </strong>{this.state.userProfileData.email}</p>}
+                        {this.state.userProfileData.handle &&
+                        <p className="text-center"><strong>Handle: </strong>{this.state.userProfileData.handle}</p>}
                     </div>
 
-                    {this.state.userProfileData.bio && <div> <h2>Bio:</h2> <p>{this.state.userProfileData.bio}</p> </div>}
+                    {this.state.userProfileData.bio && <div><h2>Bio:</h2> <p>{this.state.userProfileData.bio}</p></div>}
                     {this.state.userProfileData.itemName && <h3>{"WishList item: " + this.state.userProfileData.itemName}</h3>}
                     {this.state.userProfileData.itemCost && <h3>{"Asking for $" + this.state.userProfileData.itemCost}</h3>}
                     {this.state.userProfileData.itemDesc && <p>{this.state.userProfileData.itemDesc}</p>}
@@ -77,7 +89,7 @@ class ProfilePage extends Component {
 
                     {this.state.userProfileData.tradeOffers && <div>
                         <h2>Trade Offers:</h2>
-                        {<TradeItemList items={this.state.userProfileData.tradeOffers} />}
+                        {<TradeItemList items={this.state.userProfileData.tradeOffers}/>}
                     </div>}
 
                     {button}
